66eb291dd529e70d6ba1d7a1fa0f8c386adf14b3
uoayop <uoayop@kakao.com>
2021-07-21 22:01:10 +0900

    🔨 eslint 수정, tailwind.css 추가

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // 사용되지 않는 클래스 제거
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
