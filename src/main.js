aac9ddc01e9423b2f921ab628aff77242354707a
Gyumong <rlaalsrb3559@naver.com>
2021-08-01 20:33:21 +0900

    ✨ 소셜 로그인 카카오 구현

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import { router } from "./router";
 
 import App from "./App.vue";
 import "./assets/styles/index.css";
-
+import { KAKAO_KEY } from "./config";
 const app = createApp(App);
 
 app.use(router);
 app.mount("#app");
+window.Kakao.init(KAKAO_KEY);
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -5,7 +5,7 @@
     <Logo></Logo>
     <BrandingText></BrandingText>
     <EnterPageImg></EnterPageImg>
-    <LoginBtn></LoginBtn>
+    <LoginBtn @click="kakaoLogin"></LoginBtn>
   </div>
 </template>
 
@@ -20,6 +20,26 @@ export default {
     BrandingText,
     LoginBtn,
     EnterPageImg,
+  },
+  methods: {
+    kakaoLogin() {
+      window.Kakao.Auth.login({
+        scope: "profile, account_email",
+        success: this.getKakaoAccount,
+      });
+    },
+    getKakaoAccount() {
+      window.Kakao.API.request({
+        url: "/v2/user/me",
+        success: (res) => {
+          const kakao_account = res.kakao_account;
+          const nickname = kakao_account.profile.nickname;
+          const email = kakao_account.email;
+          console.log(nickname, "nickname");
+          console.log(email, "email");
+        },
+      });
+    },
   },
 };
 </script>
