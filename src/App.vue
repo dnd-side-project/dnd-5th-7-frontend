7e58020d2611fc467275ba11b77063f0b508d7f2
Gyumong <rlaalsrb3559@naver.com>
2021-08-01 14:08:40 +0900

    🩹 전체 view overflow-y-auto 제거

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
+<!-- @format -->
+
 <template>
   <div class="flex justify-center items-center">
-    <router-view class="w-96 h-screen bg-gray-50 overflow-y-auto"></router-view>
+    <router-view class="max-w-md w-96 h-screen bg-gray-50"></router-view>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @format */
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // 사용되지 않는 클래스 제거
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      md: "607px",
+    },
     extend: {
       spacing: {
         360: "360px",
