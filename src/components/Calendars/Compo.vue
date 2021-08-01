b3d92031ea6c856c436f3934124c98cf4868f71e
uoayop <uoayop@kakao.com>
2021-08-01 18:17:57 +0900

    🎨 달력 커스텀 (버튼 삭제, border 추가)

--- src/components/Calendars/Compo.vue
+++ src/components/Calendars/Compo.vue
@@ -12,18 +12,18 @@ export default {
 </script>
 
 <style scoped>
-:v-deep .custom-calendar.vc-container {
+::v-deep .custom-calendar.vc-container {
   border: 0;
 }
-:v-deep .custom-calendar .vc-arrow {
+::v-deep .custom-calendar .vc-arrow {
   display: none;
 }
 
-:v-deep.custom-calendar .vc-header {
+::v-deep.custom-calendar .vc-header {
   border-bottom: 2px solid #000000;
   padding: 10px 16px;
 }
-:v-deep .custom-calendar .vc-weeks {
+::v-deep .custom-calendar .vc-weeks {
   margin-top: 10px;
 }
 </style>
