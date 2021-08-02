85ceaa588470b40b266170f877124b9aa8a8276c
Gyumong <rlaalsrb3559@naver.com>
2021-08-02 19:48:09 +0900

    🎨 진입페이지 카카오 버튼 스타일링

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { router } from "./router";
-
 import App from "./App.vue";
 import "./assets/styles/index.css";
 import { KAKAO_KEY } from "./config";
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // 사용되지 않는 클래스 제거
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      "4xl": [
+        "36px",
+        {
+          letterSpacing: " -0.02em",
+          lineHeight: "44px",
+        },
+      ],
+    },
     screens: {
       md: "607px",
     },
