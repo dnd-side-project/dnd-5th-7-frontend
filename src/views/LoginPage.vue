378d8d13a262bafe75b8e953df1a8d2054d3eefc
Gyumong <rlaalsrb3559@naver.com>
2021-08-03 14:50:36 +0900

    🎨 kakao accessToken 받아오기

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -31,12 +31,16 @@ export default {
     getKakaoAccount() {
       window.Kakao.API.request({
         url: "/v2/user/me",
-        success: (res) => {
+        success: function (res) {
+          console.log(res);
+          const accessToken = window.Kakao.Auth.getAccessToken();
           const kakao_account = res.kakao_account;
           const nickname = kakao_account.profile.nickname;
           const email = kakao_account.email;
           console.log(nickname, "nickname");
           console.log(email, "email");
+          console.log(kakao_account);
+          console.log(accessToken);
         },
         fail: (err) => {
           console.log(err);
