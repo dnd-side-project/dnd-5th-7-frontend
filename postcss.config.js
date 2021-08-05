7eedb1413ad2be4e98f5ae9181742d7da5d218d5
uoayop <uoayop@kakao.com>
2021-08-06 00:23:34 +0900

    🩹 tailwind 경로 수정, 커스텀 추가

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
-const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 
 module.exports = {
   plugins: {
-    tailwindcss,
+    tailwindcss: { config: "./tailwind.config.js" },
     autoprefixer,
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+/* eslint-disable prettier/prettier */
 /** @format */
 
 module.exports = {
   purge: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // 사용되지 않는 클래스 제거
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      bold: ["Pretendard-Bold"],
+      medium: ["Pretendard-Medium"],
+      semibold: ["Pretendard-SemiBold"],
+      regular: ["Pretendard-Regular"],
+    },
     fontSize: {
       "4xl": [
         "36px",
@@ -19,9 +26,14 @@ module.exports = {
     colors: {
       gray: {
         500: "#2D2D2D",
+        200: "#D2D2D2",
       },
     },
     textColor: {
+      gray500: "#2D2D2D",
+      gray200: "#D2D2D2",
+    },
+    borderColor: {
       gray500: "#2D2D2D",
     },
     extend: {
