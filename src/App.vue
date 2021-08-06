05d78fd12a1501cfc2286448860c581fbaa0e9c3
Gyumong <rlaalsrb3559@naver.com>
2021-08-06 14:56:02 +0900

    🎨 디자인 시스템 -color 작성

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <!-- @format -->
 
 <template>
-  <div class="flex justify-center items-center bg-gray-500">
-    <router-view class="w-full mx-auto h-screen bg-white"></router-view>
+  <div class="w-full flex justify-center items-center bg-gray-500">
+    <router-view class="w-full max-w-screen-md mx-auto h-screen bg-white"></router-view>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,20 @@ module.exports = {
       md: "607px",
     },
     colors: {
-      gray: {
-        500: "#2D2D2D",
-      },
+      gray500: "#2D2D2D",
+      gray400: "#6c6c6c",
+      gray300: "#949494",
+      gray200: "#d2d2d2",
+      gray100: "#eaeaea",
+      orange: "#fa7449",
+      yellow: " #FFD74A",
+      green: "#71D76F",
+      blue: "#5E72FF",
+      pink: "#F0A7E7",
+      yellow_deep: "#AC9648",
+      black: "#000000",
+      black_dim: "rgba(0, 0, 0, 0.6)",
+      bg: "#f9f9f9",
       white: colors.white,
     },
     textColor: {
