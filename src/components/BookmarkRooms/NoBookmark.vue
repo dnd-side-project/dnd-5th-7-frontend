28591347db989e6a299e9ae39dd9c280bd1e8a32
uoayop <uoayop@kakao.com>
2021-08-07 17:17:02 +0900

    🩹 no-data 컴포넌트 경로 수정

---
+++ src/components/BookmarkRooms/NoBookmark.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-6 mt-3 mb-14 max-w-sm mx-auto flex flex-col justify-center items-center">
+    <nodataIcon class="mt-6 mb-2" />
+    <div class="font-bold text-gray500 text-center text-14">
+      아직 즐겨찾기한 기록이 없어요.<br />원하는 다이어리에서 이 아이콘을 눌러보세요!
+    </div>
+  </div>
+</template>
+
+<script>
+import nodataIcon from "../../assets/like_icon_nodata.svg";
+export default {
+  name: "NoBookmark",
+  props: {},
+  components: {
+    nodataIcon,
+  },
+};
+</script>
+
+<style scoped></style>
