335660612c9678f77e12b08376da78894632f531
uoayop <uoayop@kakao.com>
2021-08-11 23:28:22 +0900

    🎨 헤더 컴포넌트 작업 ( 방 생성 헤더, 메인 헤더 )

---
+++ src/components/Headers/AddRoomHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-row justify-between items-center p-3 h-56 header">
+    <cancleIcon @click="goback()" />
+    <div class="text-16 header-text">기록장 시작하기</div>
+    <div class="text-16 header-text" @click="goto('AddRoom2')">다음</div>
+  </div>
+  <div class="divider"></div>
+</template>
+
+<script>
+import cancleIcon from "../../assets/icon_cancle.svg";
+export default {
+  props: {},
+  components: {
+    cancleIcon,
+  },
+  methods: {
+    goback() {
+      window.history.back();
+    },
+    goto(page) {
+      this.$router.push(page);
+    },
+  },
+};
+</script>
+
+<style>
+.divider {
+  height: 2px;
+  background: #eaeaea;
+  transform: matrix(1, 0, 0, -1, 0, 0);
+}
+
+.header-text {
+}
+</style>
