100c285ce939338569deba89618678267e3ba93c
uoayop <uoayop@kakao.com>
2021-08-11 23:26:19 +0900

    ✨ 기록장 생성 페이지 생성 / 멤버 추가 버튼, 멤버 표시 컴포넌트 생성

---
+++ src/components/AddRooms/Inputs.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <form class="w-full max-w-sm p-3">
+      <div class="flex items-center border-b border-teal-500 py-2">
+        <input
+          class="my-input appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 outline-none"
+          type="text"
+          placeholder="Jane Doe"
+          aria-label="Full name"
+        />
+        <button
+          class="flex-shrink-0 border-transparent text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+          type="button"
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  </v-app>
+</template>
+
+<style>
+.my-input :focus {
+  outline: none;
+}
+</style>
---
+++ src/components/AddRooms/SelectTheme.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="font-bold text-gray500 text-18 mt-3 ml-2">테마를 선택해주세요!</div>
+  <div class="p-6 flex flex-col justify-center items-center">
+    <div class="flex flex-row">
+      <div>
+        <themeHip class="theme-icon" />
+        <div class="pl-1.5 pt-1.5 font-regular text-14">힙</div>
+      </div>
+      <div>
+        <themeModern class="theme-icon" />
+        <div class="pl-1.5 pt-1.5 font-regular text-14">모던</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import themeHip from "../../assets/SelectTheme_hip.svg";
+import themeModern from "../../assets/SelectTheme_modern.svg";
+
+export default {
+  name: "NoBookmark",
+  props: {},
+  components: {
+    themeHip,
+    themeModern,
+  },
+};
+</script>
+
+<style>
+.theme-icon {
+  width: 154px;
+  border: 1px solid #171717;
+  box-sizing: border-box;
+  margin: 0px 6px;
+}
+</style>
