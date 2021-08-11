94db39371f66cc427b406eb547669a117341666c
uoayop <uoayop@kakao.com>
2021-08-11 23:29:18 +0900

    🎨 router index.js 추가 (MyPage, RoomListPage)

---
+++ src/views/MyPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <Header></Header>
+    <div class="p-3">MyPage</div>
+  </v-app>
+</template>
+
+<script>
+import Header from "../components/Headers/MainHeader.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style></style>
