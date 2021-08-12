039f075b9b7c8ad130b8ffdf37f01b665243a3b2
uoayop <uoayop@kakao.com>
2021-08-12 22:54:07 +0900

    🎨 방 생성 두번쨰 페이지 UI 구현

---
+++ src/components/Headers/AddRoomHeader2.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex flex-row justify-between items-center p-3 h-56 header">
+    <div><backIcon @click="goback()" /></div>
+
+    <div class="text-16 header-text">기록장 시작하기</div>
+    <div></div>
+  </div>
+  <div class="divider"></div>
+</template>
+
+<script>
+import backIcon from "../../assets/icon_back.svg";
+export default {
+  data() {
+    return {
+      // headercheck: "1",
+    };
+  },
+  components: {
+    backIcon,
+  },
+  methods: {
+    goback() {
+      window.history.back();
+    },
+    goto(page) {
+      this.$router.push(page);
+    },
+  },
+};
+</script>
+
+<style>
+.divider {
+  height: 2px;
+  background: #eaeaea;
+  transform: matrix(1, 0, 0, -1, 0, 0);
+}
+
+.header-text {
+}
+</style>
--- src/views/AddRoomPage2.vue
+++ src/views/AddRoomPage2.vue
@@ -11,15 +11,23 @@
           }"
         ></BookView>
       </div>
-      <Inputs />
+      <Inputs @setName="setName()" />
+      <SelectDate @click="modalToggle" />
+      <div v-show="isModalClicked">
+        <Modal @closeModal="modalToggle" />
+      </div>
+      <div class="flex justify-center mt-20"><OKBtn /></div>
     </div>
   </v-app>
 </template>
 
 <script>
-import Header from "../components/Headers/AddRoomHeader.vue";
+import Header from "../components/Headers/AddRoomHeader2.vue";
 import BookView from "../components/AddRooms/BookView.vue";
 import Inputs from "../components/AddRooms/Inputs.vue";
+import SelectDate from "../components/AddRooms/SelectDate.vue";
+import Modal from "../components/AddRooms/CalendarModal.vue";
+import OKBtn from "../components/Common/OKButton.vue";
 
 export default {
   data() {
@@ -27,12 +35,24 @@ export default {
       name: "기록장의 이름을 적어주세요",
       date: "2021/3/4",
       theme: "hip",
+      isModalClicked: false,
     };
   },
   components: {
     Header,
     BookView,
     Inputs,
+    SelectDate,
+    Modal,
+    OKBtn,
+  },
+  methods: {
+    setName(InputName) {
+      this.name = InputName;
+    },
+    modalToggle() {
+      this.isModalClicked = !this.isModalClicked;
+    },
   },
 };
 </script>
