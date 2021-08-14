3a1228be296d55d307bec4171918331f6ed83fcb
uoayop <uoayop@kakao.com>
2021-08-14 20:31:41 +0900

    ✨ 기록장 심플 테마 레이아웃 추가

---
+++ src/components/Layout/Simple/photo/item1.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-gray500 text-white w-152 h-152 pt-10 pl-1.2 pr-16">
+    <div class="font-bold text-18 pb-10">00</div>
+    <div class="text-14 h-80 overflow-hidden overflow-ellipsis">사진1</div>
+  </div>
+</template>
---
+++ src/components/Layout/Simple/photo/item2.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-gray w-152 h-208 pt-10 pl-1.2 pr-16">
+    <div class="font-bold text-18 pb-10">00</div>
+    <div class="text-14 h-140 overflow-hidden overflow-ellipsis">사진2</div>
+  </div>
+</template>
