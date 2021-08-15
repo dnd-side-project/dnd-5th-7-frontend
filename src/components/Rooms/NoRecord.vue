b6cc6c0ff781041a30607166079faac66fb07505
Gyumong <rlaalsrb3559@naver.com>
2021-08-15 20:21:17 +0900

    🎨 백엔드랑 통신 test axios cookie

--- src/components/Rooms/NoRecord.vue
+++ src/components/Rooms/NoRecord.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="max-w-sm mx-auto flex flex-col justify-center items-center relative">
-    <nodrecordIcon class="mt-36 mb-5" />
-    <div class="font-bold text-gray200 text-center text-14">아직 기록이 없네요.<br />추억을 정리하러 가볼까요?</div>
-    <div class="h-28 flex flex-col justify-between absolute top-5 right-5">
-      <writingIcon />
-      <hostIcon />
+  <div className=" flex flex-col justify-center items-center relative z-0">
+    <nodrecordIcon class="mt-36 mb-5 z-0" />
+    {{ counter }}
+    <button @click="inc">inc</button>
+    <div class="font-bold text-gray200 text-center text-14 z-0">아직 기록이 없네요.<br />추억을 정리하러 가볼까요?</div>
+    <div class="h-28 flex flex-col justify-between absolute top-5 right-5 z-0">
+      <writingIcon class="z-0" />
+      <hostIcon class="z-0" />
     </div>
   </div>
 </template>
@@ -13,15 +15,23 @@
 import nodrecordIcon from "../../assets/plus_icon_norecord.svg";
 import writingIcon from "../../assets/floating_writing2.svg";
 import hostIcon from "../../assets/floating_host2.svg";
-export default {
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
+export default defineComponent({
   name: "NoRoom",
+  setup() {
+    const { state } = useStore();
+    const counter = computed(() => state.counter);
+    const inc = () => store.commit("setCounter");
+    return { counter, inc };
+  },
   props: {},
   components: {
     nodrecordIcon,
     writingIcon,
     hostIcon,
   },
-};
+});
 </script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { createApp } from "vue";
 import { router } from "./router";
 import SwiperCore, { Navigation, Pagination } from "swiper/core";
-
 import App from "./App.vue";
+import store from "./store";
 import "./assets/styles/index.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 SwiperCore.use([Navigation, Pagination]);
 
 app.mount("#app");
