2af1f84d164c208c4883929f7310e92c6a5158ba
Gyumong <rlaalsrb3559@naver.com>
2021-08-15 21:54:10 +0900

    🎨 kakao link 멤버 초대 링크보내기 구현

--- src/components/AddRooms/AddBtn.vue
+++ src/components/AddRooms/AddBtn.vue
@@ -4,13 +4,14 @@
       <plusIcon class="absolute plus-icon" />
     </div>
     <addBackground />
-    <div class="text-12 mt-0.8">추가</div>
+    <div class="text-12 mt-0.8" @click="sendMessage">추가</div>
   </div>
 </template>
 
 <script>
 import addBackground from "../../assets/SelectMember_background.svg";
 import plusIcon from "../../assets/SelectMember_plus.svg";
+import { FRONT_URL } from "../../config";
 
 export default {
   name: "SelectMember",
@@ -18,6 +19,31 @@ export default {
   components: {
     addBackground,
     plusIcon,
+  },
+  methods: {
+    sendMessage() {
+      window.Kakao.Link.sendDefault({
+        objectType: "feed",
+        content: {
+          title: "xx님이 xx로 초대하였습니다.",
+          description: "들어오세용",
+          imageUrl: "",
+          link: {
+            webUrl: FRONT_URL,
+            mobileWebUrl: FRONT_URL,
+          },
+          buttons: [
+            {
+              titile: "입장하기",
+              link: {
+                webUrl: FRONT_URL,
+                mobileWebUrl: FRONT_URL,
+              },
+            },
+          ],
+        },
+      });
+    },
   },
 };
 </script>
--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
 export const BASE_URL = "https://tido-diary.herokuapp.com";
+export const KAKAO_KEY = "5c9736b2b7ae9c5871b8d8f911e1751f";
+export const FRONT_URL = "https://dnd-5th-7-frontend-eight.vercel.app";
