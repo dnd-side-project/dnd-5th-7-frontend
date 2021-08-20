35b4abdfb07a3e0d7edc6a896e70ee984e8eec71
Gyumong <rlaalsrb3559@naver.com>
2021-08-21 04:35:39 +0900

    🩹 주소수정

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://tido-diary.herokuapp.com";
-export const FRONT_URL = "https://dnd-5th-7-frontend-eight.vercel.app;
+export const FRONT_URL = "https://dnd-5th-7-frontend-eight.vercel.app";
 
 // // "https://tido-diary.herokuapp.com"
 // "https://dnd-5th-7-frontend-eight.vercel.app"
