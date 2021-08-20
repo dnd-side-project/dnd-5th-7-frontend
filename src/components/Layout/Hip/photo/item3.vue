adbe81b27f3c2bf5966e3eac7cbf54a6fb941685
uoayop <uoayop@kakao.com>
2021-08-20 20:40:24 +0900

    🩹 다이어리 이미지 경로 표시

--- src/components/Layout/Hip/photo/item3.vue
+++ src/components/Layout/Hip/photo/item3.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-black w-154 h-186 layout">
     <img src="../../../../assets/layout_hip_picture_3.png" class="w-154 h-186 object-fill" />
-    <img src="../../../../data/testImage.png" class="w-124 h-124 object-cover rounded-full absolute left-16 top-28" />
+    <img :src="this.url" class="w-124 h-124 object-cover rounded-full absolute left-16 top-28" />
     <img src="../../../../assets/hip_sticker_3.png" class="w-50 h-50 absolute object-fill right-2 bottom-7" />
   </div>
 </template>
