7012b81227ba0813413a333922874eb876725555
uoayop <uoayop@kakao.com>
2021-08-20 20:43:55 +0900

    🩹 리스트뷰에서 유저 프로필 아이콘 깨지는 에러 해결 (png -> svg)

--- src/components/RoomList/Profiles.vue
+++ src/components/RoomList/Profiles.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="w-56 flex flex-row">
     <div id="p1" class="z-0">
-      <img src="../../assets/nodata_m.png" class="profileIcon" height="24px" />
+      <profile />
       <!-- 크기 왜이래!!!!!ㅠ -->
     </div>
-    <div id="p2" class="z-10 transform -translate-x-24">
-      <img src="../../assets/nodata_m.png" class="profileIcon" height="24px" />
+    <div id="p2" class="z-10 transform -translate-x-1.4">
+      <profile />
     </div>
-    <div id="p3" class="z-20 transform -translate-x-4.8">
-      <img src="../../assets/nodata_m.png" class="profileIcon" height="24px" />
+    <div id="p3" class="z-20 transform -translate-x-28">
+      <profile />
     </div>
   </div>
 </template>
 
 <script>
-// import profile from "../../assets/nodata_m.svg";
+import profile from "../../assets/nodata_m.svg";
 export default {
-  // components: { profile },
+  components: { profile },
   setup() {},
 };
 </script>
