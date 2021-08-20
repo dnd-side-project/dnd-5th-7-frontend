e76e859ab3c37f74315af7e83dbf6e04da0552ff
uoayop <uoayop@kakao.com>
2021-08-20 20:43:07 +0900

    🎨 방의 호스트에게만 호스트 버튼 보이도록 수정

--- src/components/DetailRoom/FloatingBtn.vue
+++ src/components/DetailRoom/FloatingBtn.vue
@@ -2,7 +2,7 @@
   <div class="h-122 flex flex-col justify-between absolute top-76 right-5 z-10">
     <writingIcon class="z-10 cursor-pointer" @click="Wbtnclicked" />
     <div v-show="this.WriteBtnClicked" class="absolute z-20 FirstfuncList"><WriteFunction /></div>
-    <hostIcon class="z-10 cursor-pointer" @click="Hbtnclicked" />
+    <hostIcon v-show="this.host_id == userData.id" class="z-10 cursor-pointer" @click="Hbtnclicked" />
     <div v-show="this.HostBtnClicked" class="absolute SecondfuncList z-20">
       <HostFunction :HostBtnClicked="this.HostBtnClicked" @closeModal="Hbtnclicked" />
     </div>
@@ -14,9 +14,12 @@ import WriteFunction from "./WriteFunction.vue";
 import HostFunction from "./HostFunction.vue";
 import writingIcon from "../../assets/floating_writing2.svg";
 import hostIcon from "../../assets/floating_host2.svg";
+
+import { computed } from "vue";
+import { useStore } from "vuex";
 export default {
   components: { writingIcon, hostIcon, HostFunction, WriteFunction },
-  setup() {},
+  props: ["host_id"],
   data() {
     return {
       WriteBtnClicked: false,
@@ -32,6 +35,13 @@ export default {
       // console.log("Hbtnclicked");
       this.HostBtnClicked = !this.HostBtnClicked;
     },
+  },
+  setup() {
+    const store = useStore();
+    const userData = computed(() => {
+      return store.getters[`userStore/getUser`];
+    });
+    return { userData };
   },
 };
 </script>
